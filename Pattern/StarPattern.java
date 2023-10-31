c5d4560502f3e63bb91b02643747cc07805515a1
Piyush631 <125112921+Piyush631@users.noreply.github.com>
2023-10-31 12:19:02 +0530

    Add files via upload

---
+++ Pattern/StarPattern.java
@@ -0,0 +1,18 @@
+package Pattern;
+public class StarPattern{
+    public static void main(String args[])
+    {
+        //outer loop
+        for(int i = 1 ; i <= 4; i++)
+        {
+            //inner loop
+            for(int j=1; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+       
+
+    }
+}
