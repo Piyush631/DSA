fb964eca84cdf15d7b4240215acf35dc18502615
Piyush631 <125112921+Piyush631@users.noreply.github.com>
2023-11-09 21:11:00 +0530

    Add files via upload

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -1,8 +1,28 @@
+package Arrays;
+import java.util.*;
+
 public class Practice {
+    public static void run(int number[],int target)
+    {
+        for(int i=0;i<number.length;i++)
+        {
+            for(int j=i+1;j<number.length;j++)
+            {
+                if(number[i]+number[j]==target)
+                {
+                    System.out.println("("+i+", " +j+")");
+                }
+            }
+        }
+    }
     public static void main(String args[])
     {
-        int a []={2,4,5,6};
-        System.out.println(a.length);
+         int number[]={2,3,7,15};
+    int target=9;
+run(number,target);
     }
+   
+      
+        
     
 }
